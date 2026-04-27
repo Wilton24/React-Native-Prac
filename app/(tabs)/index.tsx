33d7d19f95dbb6cebda43fe8ec6f8f73c73dc085
Wilton Batiquin <cbasher24@gmail.com>
2026-04-27 09:41:43 +0800

    Update App component text and enhance global styles with Tailwind CSS configurations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
+import "@/global.css";
 import { Text, View } from "react-native";
-import "../../global.css";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-slate-900">
       <View className="p-6 bg-blue-500 rounded-3xl border-4 border-blue-300 shadow-xl">
         <Text className="text-white text-2xl font-bold tracking-widest uppercase">
-          NativeWind v5 is Working! 🚀
+          NativeWind v5 is Working in da Haus! 🚀
         </Text>
       </View>
       <Text className="mt-4 text-slate-400 font-medium">
